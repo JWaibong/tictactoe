08117eee314807b3febd8918e163bddb681d4ca8
Jonathan Ng <jonathan.ng@stonybrook.edu>
2021-06-10 23:51:27 -0400

    project is fully functional

--- script.js
+++ script.js
@@ -1,23 +1,80 @@
 let gameBoard = (function(){
     "use-strict";
-    let currentSpace = null;
+    let _currentSpace = null;
+    let _moves = 0;
     let _board = [null, null, null,
                   null, null, null,
                   null, null, null];
     function setBoard(value){
-        _board[currentSpace] = value;
+        if(_board[_currentSpace] === null){
+            _board[_currentSpace] = value;
+            _moves++;
+            p1.toggleTurn();
+            p2.toggleTurn();
+        }
+
     }
     function setCurrentSpace(index){
-        currentSpace = index;
+        _currentSpace = index;
     }
     function showCurrentSpace(){
-        console.log(currentSpace);
+        console.log(_currentSpace);
     }
 
     function showBoard(){
         console.log(_board);
     }
-    return{ setBoard, showBoard, setCurrentSpace, showCurrentSpace,};
+    function updateBoard(){
+        // gridSpaces.forEach((grid,index) => grid.textContent = (_board[index] === null)? "" : (_board[index] === 1)? "X": "O");
+        if(_currentSpace != null){
+            const gridToUpdate = document.getElementById(`grid${_currentSpace}`);
+            gridToUpdate.textContent = (_board[_currentSpace] === 1)? "X" : "O";
+        }
+    }
+    function _checkWin(){
+        let diag = _checkDiagonals();
+        let rows = _checkRows();
+        let columns = _checkColumns();
+        return ((diag !== null)? diag: (rows !== null)? rows: (columns !== null)? columns : (_moves >= 9)? "Tie": null);
+    }
+    function _checkDiagonals(){
+        if(_board[4] != null){
+           return ((_board[0] === _board[4] && _board[4] === _board[8]) || (_board[2] === _board[4] && _board[4] === _board[6]))? _board[4] : null;
+        }
+        return null;
+    }
+    function _checkRows(){
+        for(let i=0; i<=6; i+=3){
+            if(_board[i] != null){
+                if(_board[i] === _board[i+1] && _board[i+1] ===  _board[i+2]){
+                    return _board[i];
+                }
+            }
+        }
+        return null;
+    }
+    function _checkColumns(){
+        for(let i=0; i<3; i++){
+            if(_board[i] != null){
+                if(_board[i] === _board[i+3] && _board[i+3] ===  _board[i+6]){
+                    return _board[i];
+                }
+            }
+        }
+        return null; 
+    }
+    function determineWinner(){
+        const player = _checkWin();
+        document.querySelector("#winner").textContent = (player == 1)? "X is the Winner!" : (player == 0)? "O is the Winner!" : (player === "Tie")? player : null;
+    }
+    function reset(){
+        _moves = 0; 
+        _currentSpace = null;
+        _board.forEach((value,index) => _board[index] = null);
+        document.querySelectorAll(".board").forEach(grid => grid.textContent = "");
+        document.querySelector("#winner").textContent = "";
+    }
+    return{setBoard, showBoard, setCurrentSpace, showCurrentSpace, updateBoard, determineWinner,reset};
     
 }());
 
@@ -32,19 +89,20 @@ const Player = (name) => {
 
 const p1 = Player("Player 1");
 p1.toggleTurn();
-console.log(p1.isMyTurn());
 
 const p2 = Player("Player 2");
-console.log(p2.isMyTurn());
 
 let gridSpaces = document.querySelectorAll(".board");
 gridSpaces.forEach((grid,index) => {
     grid.addEventListener("click", () => {
         gameBoard.setCurrentSpace(index);
         gameBoard.setBoard(p1.isMyTurn());
-        p1.toggleTurn();
-        p2.toggleTurn();
-        gameBoard.showCurrentSpace();
-        gameBoard.showBoard();
+        gameBoard.updateBoard();
+        gameBoard.determineWinner()
     });
+});
+
+let reset = document.querySelector("#reset");
+reset.addEventListener("click", e => {
+    gameBoard.reset();
 });
